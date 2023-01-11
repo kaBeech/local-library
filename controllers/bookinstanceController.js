e986c6d004a572c610393f3999d01fb5f4298442
kaBeech <contact@kaBeech.com>
2023-01-11 15:57:57 -0800

    Add sorting to book-instance list

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -4,6 +4,7 @@ const BookInstance = require("../models/bookinstance");
 exports.bookinstance_list = function (req, res, next) {
   BookInstance.find()
     .populate("book")
+    .sort([["book", "ascending"]])
     .exec(function (err, list_bookinstances) {
       if (err) {
         return next(err);
