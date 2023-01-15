364b6d158a9ddb1c13be45820699c61ddd71c48c
kaBeech <contact@kaBeech.com>
2023-01-15 13:36:09 -0800

    Prepare archival copy

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB =
-  "mongodb+srv://TestDB:S80s33tA2FXBmcf3@cluster0.iy0xnr9.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
